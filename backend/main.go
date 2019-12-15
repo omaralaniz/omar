8af39b5e3b5b23dfb887d9a735124862e35465bf
omaralaniz <omar.alaniz021@gmail.com>
2019-12-14 16:52:17 -0800

    Added go.mod to create a module. eliminating relative ugly paths! also changed names of functions, imports, and files for ease of readability

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"./handlers"
-	"./repository"
+	handler "github.com/omaralaniz/omar/handlers"
+	repo "github.com/omaralaniz/omar/repository"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	projects := repository.New()
+	projects := repo.New()
 
 	api := r.Group("/api")
-	api.GET("/ping", handlers.Ping())
-	api.GET("/projects", handlers.GetAll(projects))
+	api.GET("/ping", handler.Ping())
+	api.GET("/projects", handler.GetAll(projects))
 
 	err := r.Run("0.0.0.0:8080")
 
